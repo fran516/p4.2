5f98775f118e7b89b57de6363da83a3174925b91
fran516 <rzq512@york.ac.uk>
2025-10-16 17:11:55 +0100

    16/10/25

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject target;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        target = GameObject.Find ("Car2");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.LookAt (target.transform);
+    }
+}
--- Assets/TopDownCamera.cs
+++ Assets/TopDownCamera.cs
@@ -1,17 +1,17 @@
 using UnityEngine;
 
-public class TopDown : MonoBehaviour
+public class TopDownCamera : MonoBehaviour
 {
     public GameObject target;
     Vector3 offset;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        offset = transform.position - target.transform.position;
+       offset = transform.position - target.transform.position; 
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         Vector3 newPos = target.transform.position + offset;
         transform.position = newPos;
